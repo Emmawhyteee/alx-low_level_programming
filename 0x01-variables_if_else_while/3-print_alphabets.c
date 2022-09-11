b500b67f348d0455eb410985454425e0cecbc4a3
Emmawhyteee <111117235+Emmawhyteee@users.noreply.github.com>
2022-09-11 13:34:31 -0700

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,15 +6,15 @@
  *  * main - Entry point
  *   *
  *    * Return: Always 0 (Success)
- *     */
+ **/
 
 int main(void)
 {
 		char low;
 
-			for (low = 'a'; low <= 'z'; low++)
+			for (low = 'a'; low <= 'z'; low++);
 						putchar(low);
-				for (low = 'A'; low <= 'Z'; low++)
+				for (low = 'A'; low <= 'Z'; low++);
 							putchar(low);
 					putchar('\n');
 
