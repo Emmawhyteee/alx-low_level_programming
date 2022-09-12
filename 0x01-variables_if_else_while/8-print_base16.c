6761e701f0d8b4241d2cd6506b90480f4f68755a
Emmawhyteee <111117235+Emmawhyteee@users.noreply.github.com>
2022-09-11 17:07:00 -0700

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int d = 48;
-	char c = 'a';
+	int d;
 
-	while (d < 58)
+	for (d = '0'; d <= '9'; d++)
 	{
 		putchar(d);
-		d++;
-	}
-	while (c <= 'f')
-	{
-		putchar(c);
-		c++;
+		if (d != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 
