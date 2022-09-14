288dfa89897f6a3dcba26067b46acf7a94287f2c
Emmawhyteee <111117235+Emmawhyteee@users.noreply.github.com>
2022-09-14 11:51:23 -0700

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,39 +1,50 @@
- #include "main.h"
-#include <stdio.h>
+#include "main.h"
 
 /**
- *  * print_to_98 - prints all the integers between the number given and 98
- *   * @n: number to count from
- *    *
- *     * Return: void
- *      */
+ * print_times_table - Print the `n` times table, starting with 0.
+ * Description: If `n` is greater than 15 or less than 0, print nothing.
+ * @n: int type number
+ */
 
-void print_to_98(int n)
+void print_times_table(int n)
 {
-		if (n > 98)
-				{
-							while (n >= 98)
-										{
-														printf("%d", n);
-																	if (n != 98)
-																					{
-																										printf(", ");
-																													}
-																				n--;
-																						}
-								}
-			else
-					{
-								while (n <= 98)
-											{
-															printf("%d", n);
-																		if (n != 98)
-																						{
-																											printf(", ");
-																														}
-																					n++;
-																							}
-									}
-				printf("\n");
-}
+	int x = 0, y, z;
 
+	if (n > 15 || n < 0)
+		return;
+	while (x <= n)
+	{
+		for (y = 0; y <= n; y++)
+		{
+			z = x * y;
+			if (z > 99)
+			{
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (z > 9)
+			{
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+				_putchar(z + '0');
+
+			if (y != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+		x++;
+	}
+}
