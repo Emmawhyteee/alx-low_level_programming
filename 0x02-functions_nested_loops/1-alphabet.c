0e6092ce63f271341e7da17309b68f24055ff745
Emmawhyteee <111117235+Emmawhyteee@users.noreply.github.com>
2022-09-14 05:42:26 -0700

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,24 @@
- #include "main.h"
+ #include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
 /**
- *  * print_alphabet -  prints the lowercase alphabet
- *   *
- *    * Return: void
- *     */
-
-void print_alphabet(void)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-		char c = 'a';
-			int i;
+	int n, last_d;
 
-				for (i = 0; i < 26; i++)
-						{
-									_putchar(c + i);
-										}
-					_putchar(10);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_d = n % 10;
+	if (last_d > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_d);
+	else if (last_d == 0)
+		printf("Last digit of %d is %d and is 0\n", n, last_d);
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_d);
+	return (0);
 }
-
