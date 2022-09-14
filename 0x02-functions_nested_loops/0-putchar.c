439ff14c2ee5a912b57b27378626b01d2056e8cf
Nwankwo Emmanuel <emmatexiii@gmail.com>
2022-09-14 04:41:20 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,43 @@
 #include "main.h"
 
+
+
 /**
- * main - prints the word _putchar
  *
- * Return: 0 on success execution.
- */
+ *  * main - prints the word _putchar
+ *
+ *   *
+ *
+ *    * Return: 0 on success execution.
+ *
+ **/
+
+
 
 int main(void)
+
 {
-	char s[] = "_putchar";
-	int i = 0;
 
-	while (s[i] != '\0')
-	{
-		_putchar(s[i]);
-		i++;
-	}
-	_putchar(10);
+		char s[] = "_putchar";
 
-	return (0);
+			int i = 0;
+
+
+
+				while (s[i] != '\0')
+
+						{
+
+									_putchar(s[i]);
+
+											i++;
+
+												}
+
+					_putchar(10);
+
+
+
+						return (0);
+
 }
