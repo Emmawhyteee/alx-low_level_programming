f582c273ecfb9fec262c5f4ac63985bc46e61f88
Emmawhyteee <111117235+Emmawhyteee@users.noreply.github.com>
2022-09-15 16:05:22 -0700

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,43 +1,22 @@
 #include "main.h"
 
-
-
 /**
+ * print_line - prints line using "_" character of n length
+ *@n: length of the line
  *
- *  * print_line - prints line using "_" character of n length
- *
- *   *@n: length of the line
- *
- *    *
- *
- *     * Return: void
- *
- *      */
-
+ * Return: void
+ */
 void print_line(int n)
-
 {
+	int i = 0;
 
-		int i = 0;
-
-
-
-			if (n > 0)
-
-					{
-
-								while (i < n)
-
-											{
-
-															_putchar(95);
-
-																		i++;
-
-																				}
-
-									}
-
-				_putchar('\n');
-
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			_putchar(95);
+			i++;
+		}
+	}
+	_putchar('\n');
 }
