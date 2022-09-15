873095add3b64863cfc1dbcbe9db64c4187cee47
Emmawhyteee <111117235+Emmawhyteee@users.noreply.github.com>
2022-09-15 15:44:08 -0700

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- * _isdigit - checks for a digit (0 through 9)
- * @c: digit to be checked
- * Return: 1 if c is a digit, otherwise 0
+ * mul - returns result of multiplication between 2 integers
+ *@a: first multiplier
+ *@b: second multiplier
+ *
+ * Return: multiplication result
  */
-int _isdigit(int c)
+int mul(int a, int b)
 {
-	if ((c >= 48) && (c <= 57))
-		return (1);
-	else
-		return (0);
+	int result;
+
+	result = a * b;
+	return (result);
 }
