fa4e860b11807ab1ae9c77628d60f14808ac3300
Emmawhyteee <111117235+Emmawhyteee@users.noreply.github.com>
2022-09-20 10:48:56 -0700

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,16 +8,16 @@
  */
 void puts_half(char *str)
 {
-	int i;
-	int oE = 0;
+int i;
+int oE = 0;
 
-	if (_strlen(str) % 2 != 0)
-	{
-		oE += 1;
-	}
-	for (i = (_strlen(str) + oE) / 2; i < _strlen(str); i++)
-	{
-		_putchar(str[i]);
-	}
-	        _putchar('\n');
+if (_strlen(str) % 2 != 0)
+{
+oE += 1;
+}
+for (i = (_strlen(str) + oE) / 2; i < _strlen(str); i++)
+{
+_putchar(str[i]);
+}
+ _putchar('\n');
 }
