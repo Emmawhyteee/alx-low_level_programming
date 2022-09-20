7a2b1cd14b180da5d117e94c5975a275a9da47a9
Nwankwo Emmanuel <emmatexiii@gmail.com>
2022-09-20 09:37:15 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+#include "2-strlen.c"
+
+/**
+ *
+ *  * _strcpy - main function to copy
+ *
+ *   *
+ *
+ *    * @dest: destination to copy
+ *
+ *     * @src: src
+ *
+ *      *
+ *
+ *       * Return: a character value
+ *
+ *        */
+
+char *_strcpy(char *dest, char *src)
+
+{
+
+		int i;
+
+
+
+			for (i = 0; i <= _strlen(src); i++)
+
+					{
+
+								dest[i] = src[i];
+
+									}
+
+				return (dest);
+
+}
+
+Footer
+
+© 2022 GitHub, Inc.
+
+Footer navigation
+
+Terms
+
+Privacy
+
