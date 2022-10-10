6a529d28eaa6e1739ba2440f9bc8c41c2b8bc5e5
Emmawhyteee <111117235+Emmawhyteee@users.noreply.github.com>
2022-10-10 14:20:45 -0700

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,1 +1,14 @@
-njb
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print the the name of the file it was compiled from,
+ * followed by a new line
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
