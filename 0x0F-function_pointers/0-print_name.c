14f8ee74f3c3f915b44cb3fff0855d8d59950906
Emmawhyteee <111117235+Emmawhyteee@users.noreply.github.com>
2022-10-12 14:47:57 -0700

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
-  * array_iterator - print name
-  * @array: pointer to an anrray
-  * @size: size of array
-  * @action: pointer to function
+  * print_name - print name
+  * @name: pointer to a char
+  * @f: pointer to a funtion
   */
-void array_iterator(int *array, size_t size, void (*action)(int))
-{
-	 size_t i;
 
-	if (array != NULL && action != NULL)
-	{
-		for (i = 0; i < size; i++)
-		action(array[i]);
-	}
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	f(name);
 }
