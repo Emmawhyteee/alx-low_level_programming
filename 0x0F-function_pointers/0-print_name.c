f7e63604a074b436c2200813f7c9bb768ff069ea
Emmawhyteee <111117235+Emmawhyteee@users.noreply.github.com>
2022-10-12 08:00:09 -0700

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,14 @@
-avchjv
+#include "function_pointers.h"
+
+/**
+ * print_name - prints name through passed in function pointer
+ *
+ * @name: character string
+ * @f: function pointer
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f)
+		f(name);
+}
