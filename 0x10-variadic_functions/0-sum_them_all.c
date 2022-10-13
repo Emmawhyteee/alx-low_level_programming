1ed918ccf9067c8dd43a2451c0ed6767b71a40f5
Nwankwo Emmanuel <emmatexiii@gmail.com>
2022-10-13 11:14:00 -0700

    Beauty is variable, ugliness is constant

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ *
+ *  * main - check the code
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
+int main(void)
+
+{
+	int sum;
+
+	sum = sum_them_all(30,40,50);
+	printf("%u\n",sum);
+	return 0;
+}
