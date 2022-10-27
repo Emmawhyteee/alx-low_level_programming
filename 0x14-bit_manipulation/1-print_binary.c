e509d72d226cbc10b906b3cf14cac82473470b1b
Emmawhyteee <111117235+Emmawhyteee@users.noreply.github.com>
2022-10-27 11:50:44 -0700

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
 *print_binary - prints the binary representation of a number
 *@n: number to be printed
 *
 *Return: void
 */
-
 void print_binary(unsigned long int n)
 {
 if (n > 1)
